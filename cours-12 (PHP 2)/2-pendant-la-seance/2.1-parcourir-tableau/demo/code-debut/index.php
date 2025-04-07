36c4752388acb86d87036ac5b9613760025b2752
Daniel Parpal <daniel.parpal@hotmail.com>
2025-04-07 19:25:56 -0400

    Avec TP3

--- cours-12 (PHP 2)/2-pendant-la-seance/2.1-parcourir-tableau/demo/code-debut/index.php
+++ cours-12 (PHP 2)/2-pendant-la-seance/2.1-parcourir-tableau/demo/code-debut/index.php
@@ -4,13 +4,25 @@
 // auxquels un client a participé.
 
 // Définition du tableau des titres de cours
+$titres = [
+	"X-Fit",
+	"Yoga flow avec musique",
+	"Bootcamp et course"
+];
 
+// On peut utiliser var_dump() pour afficher le tableau à des
+// fins de diagnostic
+// var_dump($titres);
 
 // Mettons en place une boucle pour afficher tous les titres (sans HTML pour débuter)
 // Nous commenterons cette section par la suite
+// foreach ($titres as $titre) {
+// 	echo $titre . "<br>";
+// }
 
 
 // Effectuons l'inclusion du fichier d'affichage
-
+include 'app.html.php';
 
 ?>
+
